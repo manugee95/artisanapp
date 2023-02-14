033fff558e7b7339838f7d3623c7648255fbce20
unknown <muhammedadam4544@gmail.com>
2023-02-14 14:14:33 +0100

    task form

--- src/js/components/create-task.js
+++ src/js/components/create-task.js
@@ -1,11 +1,40 @@
 init(() => {
 	Alpine.data("createTask", () => ({
 		currentStep: 1,
-		nextStep() {
-			this.currentStep++;
+		taskInfo: {
+			title: "",
+			taskDate: "",
+			address: "",
+			summary: "",
+			budget: 0,
 		},
+
+		nextStep(el) {
+			if (el !== undefined) {
+				if (this.currentStep === 4) {
+					el.submit();
+				} else {
+					this.currentStep++;
+				}
+			}
+		},
+
 		prevStep() {
 			this.currentStep--;
+		},
+
+		initDatePicker(input, container) {
+			flatpickr(input, {
+				appendTo: container,
+				enableTime: true,
+				dateFormat: "Z",
+				altInput: true,
+				altFormat: "D j M, h:i K",
+				minDate: "today",
+				onChange: (_, dateStr) => {
+					this.taskInfo.taskDate = dateStr;
+				},
+			});
 		},
 	}));
 });
